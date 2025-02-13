1d771a9323c07dbc0ffbe4d1094a2848bd41300f
Weigand <Marcel.Weigand@t-systems.com>
2025-02-13 21:42:48 +0100

    correct pipeline

--- backend.tf
+++ backend.tf
@@ -1,8 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket         = "my-terraform-remote-state"
-    key            = "s3-bucket/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
-  }
+  backend "s3" terraform {
+  backend "s3" {}
+}
 }
